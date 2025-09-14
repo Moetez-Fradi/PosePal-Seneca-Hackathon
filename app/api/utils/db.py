5a7965c49d8f6fc4a8d4e8558e392778c9673920
ghassen naouar <ghassennaouar7@gmail.com>
2025-09-14 01:26:21 +0100

    enabling SSL/TLS

--- app/api/utils/db.py
+++ app/api/utils/db.py
@@ -6,6 +6,10 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME")
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    tls=True,  # enable SSL/TLS
+    tlsAllowInvalidCertificates=True  # only for local dev/testing, remove in production
+)
 db = client[DB_NAME]
 users_collection = db["users"]
